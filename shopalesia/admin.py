35feb7746f36cafa8d50819d57a840826ce8f9c3
Alesia <alesik.sur@gmail.com>
2022-09-27 16:56:04 +0300

    create shop

--- shopalesia/admin.py
+++ shopalesia/admin.py
@@ -7,16 +7,23 @@ from .models import Category, Product, Order
 class OrderAdmin(admin.ModelAdmin):
     empty_value_display = 'H/Y'
     list_display = ('user', 'product', 'date_created', 'is_paid')
+    list_filter = ('user', 'product', 'date_created', 'is_paid')
 
 # admin.site.register(Category)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     empty_value_display = 'H/Y'
     list_display = ('category', 'name', 'price', 'stock')
+    list_filter = ('category', 'name', 'price', 'stock')
+    search_fields = ('name', 'price')
+    search_help_text = 'введите категорию и продукт'
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     empty_value_display = 'H/Y'
     list_display = ('name', 'is_published', 'id')
+    list_filter = ('name', 'is_published')
+    search_fields = ('name', 'is_published')
+    search_help_text = 'введите категорию'
 
 # Register your models here.
